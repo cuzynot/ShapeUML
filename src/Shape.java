e4416c8daddbd525adf36e5bc0fadb6a9846f6ea
veggieman420 <joshdoesstuff4fun@gmail.com>
2018-09-10 10:40:34 -0400

    Shape methods

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,29 @@
+import java.awt.Rectangle;
 
-public class Shape {
-	double x,y;
+abstract public class Shape {
+	private double x,y;
+	private Rectangle boundingBox = new Rectangle();
+	
+	Shape(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	abstract public double getArea();
+	
+	public void setX(double num){
+		this.x = num;
+	}
+	
+	public void setY(double num){
+		this.y = num;
+	}
+	
+	public double getX(){
+		return this.x;
+	}
+	
+	public double getY(){
+		return this.y;
+	}
 }
